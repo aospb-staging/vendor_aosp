65c3e60ae045813e0732beb2fe590ecb74c19e20
Danny Lin <danny@kdrag0n.dev>
2021-06-23 01:22:07 -0700

    vendorsetup: Override host metadata to avoid leaking info

---
+++ vendorsetup.sh
@@ -0,0 +1,6 @@
+# Shebang is intentionally missing - do not run as a script
+
+# Override host metadata to make builds more reproducible and avoid leaking info
+export BUILD_USERNAME=AOSP
+export BUILD_HOSTNAME=android-build
+
