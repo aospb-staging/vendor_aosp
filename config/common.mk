1f3413589e36f9a4b9f4f1130def718455ecaeed
AdarshGrewal <adarshgrewal@gmail.com>
2021-11-22 00:08:03 +0530

    common: Enable gestural navbar and systemuiGX overlays by default
    * Taken from Pixel 6 Pro
    
    Change-Id: I3804edf648cb80d40fd3c3252922144626210436

--- config/common.mk
+++ config/common.mk
@@ -155,6 +155,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/aosp/prebuilt/common/etc/init/init.openssh.rc:$(TARGET_COPY_OUT_PRODUCT)/etc/init/init.openssh.rc
 
+# Overlay
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.boot.vendor.overlay.theme=com.android.internal.systemui.navbar.gestural;com.google.android.systemui.gxoverlay
+
 # rsync
 PRODUCT_PACKAGES += \
     rsync
