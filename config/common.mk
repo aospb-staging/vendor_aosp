f315ec37a2cb65f7e6eed7a7941ab4f2f84b7ade
jhenrique09 <jhsv09@gmail.com>
2022-06-08 12:58:01 +0000

    overlay: Fix app icon font on PixelLauncher
    Change-Id: Id98d40ff645d268d653f06d6761200d03ad7d668

--- config/common.mk
+++ config/common.mk
@@ -181,6 +181,7 @@ PRODUCT_PACKAGE_OVERLAYS += \
     vendor/aosp/overlay/common
 
 PRODUCT_PACKAGES += \
+    CustomFontPixelLauncherOverlay \
     DocumentsUIOverlay \
     NetworkStackOverlay \
     PermissionControllerOverlay
