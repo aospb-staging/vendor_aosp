c9297f6f77203107fa0ba036b483a545cdab6e49
dianlujitao <dianlujitao@lineageos.org>
2025-02-02 19:06:49 +0800

    kernel: Automatically adapt path suffix for GKI modules
    It has been noticed that sm8750 no longer puts GKI modules in the
    subdirectory. Moreover, this seems to be something specific to QCOM,
    so guard it as such.
    
    Change-Id: Iac9c895c9c1eef06b2e4ce75bd7c5c2aa356fbb1

--- build/tasks/kernel.mk
+++ build/tasks/kernel.mk
@@ -106,7 +106,13 @@ KERNEL_CONFIG := $(KERNEL_OUT)/.config
 KERNEL_RELEASE := $(KERNEL_OUT)/include/config/kernel.release
 RECOVERY_KERNEL_CONFIG := $(RECOVERY_KERNEL_OUT)/.config
 RECOVERY_KERNEL_RELEASE := $(RECOVERY_KERNEL_OUT)/include/config/kernel.release
-GKI_SUFFIX := $(shell echo android$(PLATFORM_VERSION)-$(TARGET_KERNEL_VERSION))
+ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
+ifeq ($(shell expr $(TARGET_KERNEL_VERSION) \>= 5.15), 1)
+ifeq ($(shell expr $(TARGET_KERNEL_VERSION) \<= 6.1), 1)
+GKI_SUFFIX := /$(shell echo android$(PLATFORM_VERSION)-$(TARGET_KERNEL_VERSION))
+endif
+endif
+endif
 
 ifeq ($(KERNEL_ARCH),x86_64)
 KERNEL_DEFCONFIG_ARCH := x86
