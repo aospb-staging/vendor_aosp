327931a14782d533677a21e087a27953b323bd1e
Arindam Bhattacharjee <abhattacharjee717@gmail.com>
2025-06-19 19:50:55 +0000

    vendor: Add faceunlock support
    Change-Id: Ie5666ae87c1ff7c5b24fe45ac71989fb9e472d6f
    Signed-off-by: Arindam Bhattacharjee <abhattacharjee717@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -114,6 +114,15 @@ PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
     system/bin/getcap \
     system/bin/setcap
 
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED ?= $(TARGET_SUPPORTS_64_BIT_APPS)
+ifeq ($(TARGET_FACE_UNLOCK_SUPPORTED),true)
+PRODUCT_PACKAGES += ParanoidSense
+PRODUCT_SYSTEM_EXT_PROPERTIES += ro.face.sense_service=true
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
+endif
+
 # Filesystems tools
 PRODUCT_PACKAGES += \
     fsck.ntfs \
