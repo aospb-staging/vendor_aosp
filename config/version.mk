d37f7f0c647e2d38a82aa4a9dfe85fa40899af7c
Sajid Shahriar <63386478+sajidshahriar72543@users.noreply.github.com>
2025-10-17 10:05:58 +0000

    vendor: Remove unneccessary OTA string from zip name

--- config/version.mk
+++ config/version.mk
@@ -2,7 +2,7 @@ CURRENT_DEVICE=$(shell echo "$(TARGET_PRODUCT)" | cut -d'_' -f 2,3)
 
 AOSP_BUILD_TYPE ?= COMMUNITY-BUILD
 
-AOSP_VERSION := aosPB-$(CURRENT_DEVICE)-OTA-$(shell date -u +%Y%m%d-%H%M)
+AOSP_VERSION := aosPB-$(CURRENT_DEVICE)-$(shell date -u +%Y%m%d-%H%M)
 
 # AOSP version properties
 PRODUCT_SYSTEM_PROPERTIES += \
