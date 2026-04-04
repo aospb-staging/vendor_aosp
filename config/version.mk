ProjectPixelage/android_vendor_lineage

vendor: Allow setting maintainer string with a flag

config/version.mk
@@ -19,6 +19,7 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.pixelage.build.version=$(PIXELAGE_DISPLAY_VERSION) \
     ro.pixelage.device=$(PIXELAGE_BUILD) \
     ro.pixelage.fingerprint=$(ROM_FINGERPRINT) \
+    ro.pixelage.maintainer=$(PIXELAGE_MAINTAINER) \
     ro.pixelage.releasetype=$(PIXELAGE_BUILDTYPE) \
     ro.pixelage.version=$(PIXELAGE_VERSION) \
     ro.modversion=$(PIXELAGE_VERSION)

aospb-staging/vendor_aosp

Change-Id: Id95456f04ac051d1797836a1e593bc55f6966b3d

config/version.mk
@@ -6,6 +6,7 @@ AOSP_VERSION := aosPB-$(CURRENT_DEVICE)-$(shell date -u +%Y%m%d-%H%M)
 
 # AOSP version properties
 PRODUCT_SYSTEM_PROPERTIES += \
+    ro.aospb.maintainer=$(AOSPB_MAINTAINER) \
     ro.aosp.version=$(AOSP_VERSION) \
     ro.aosp.releasetype=$(AOSP_BUILD_TYPE) \
     ro.aosp.build.version=$(CURRENT_DEVICE)-$(shell date -u +%Y%m%d-%H%M)